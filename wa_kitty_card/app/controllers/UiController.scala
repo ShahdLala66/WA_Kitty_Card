25f963a1608acfb8de7c2882c5fb6fb630946efd
shahdlala66 <shahd.lala66@gmail.com>
2025-10-17 22:34:11 +0200

    feat: Grid has its own page now

--- wa_kitty_card/app/controllers/UiController.scala
+++ wa_kitty_card/app/controllers/UiController.scala
@@ -16,4 +16,29 @@ class UiController @Inject()(cc: ControllerComponents) extends AbstractControlle
     def HelloKitty: Action[AnyContent] = Action {
         Ok(views.html.HelloKitty("Hello, Kitty Card Game!"))
     }
-}
+
+    def gridColors: Action[AnyContent] = Action {
+        val gridData = Main.controller.getGridColors
+        
+        // Transform the grid data into a browser-friendly format
+        val gridWithHtmlColors = gridData.map { case (x, y, card, suitColor) =>
+            val htmlColor = suitColor.toString.toLowerCase match {
+                case "green" => "#4CAF50"
+                case "brown" => "#8B4513"
+                case "purple" => "#9370DB"
+                case "blue" => "#2196F3"
+                case "red" => "#F44336"
+                case "white" => "rgba(69, 69, 69, 1)"
+                case _ => "#CCCCCC"
+            }
+            
+            val cardInfo = card.map(c => s"${c.toString}").getOrElse("Empty")
+            val suitName = suitColor.toString
+            
+            (x, y, cardInfo, htmlColor, suitName)
+        }
+        
+        Ok(views.html.gridColors(gridWithHtmlColors))
+    }
+  
+    }
