63842beee96c221a58e3c41a615f71fd697e920f
LittleDemonh <cor.eulitz@gmail.com>
2025-12-13 16:55:01 +0100

    feat: pwa works

--- wa_kitty_card/frontend/vue.config.js
+++ wa_kitty_card/frontend/vue.config.js
@@ -12,23 +12,46 @@ module.exports = {
   },
   configureWebpack: {
     plugins: [
-      new GenerateSW({
+            new GenerateSW({
         skipWaiting: true,
         clientsClaim: true,
         runtimeCaching: [
           {
-            urlPattern: new RegExp('^http://localhost:9000/'),
+            // API: relativ, funktioniert lokal und auf einer späteren Domain
+            urlPattern: ({ url }) => url.pathname.startsWith('/api'),
             handler: 'NetworkFirst',
             options: {
               cacheName: 'api-cache',
               networkTimeoutSeconds: 10,
               expiration: {
                 maxEntries: 50,
-                maxAgeSeconds: 5 * 60, // 5 minutes
+                maxAgeSeconds: 5 * 60,
               },
-              cacheableResponse: {
-                statuses: [0, 200],
+              cacheableResponse: { statuses: [0, 200] },
+            },
+          },
+          {
+            // JS/CSS/Fonts: schnell laden, aber bei Änderungen frisch
+            urlPattern: ({ request }) =>
+              request.destination === 'style' ||
+              request.destination === 'script' ||
+              request.destination === 'font',
+            handler: 'StaleWhileRevalidate',
+            options: {
+              cacheName: 'static-resources',
+            },
+          },
+          {
+            // Bilder: Cache-first mit Ablauf
+            urlPattern: ({ request }) => request.destination === 'image',
+            handler: 'CacheFirst',
+            options: {
+              cacheName: 'image-cache',
+              expiration: {
+                maxEntries: 60,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Tage
               },
+              cacheableResponse: { statuses: [0, 200] },
             },
           },
         ],
